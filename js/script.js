0743269c5f81bd845469fb18bd58dfc683d41786
TamHajiy <78784747+TamHajiy@users.noreply.github.com>
2021-06-23 13:51:54 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,21 +10,68 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+//Array of objects
+const quotes = [
+  {
+    quote:'Be yourself; everyone else is already taken.', 
+    source:'Oscar Wilde'
+  },
+  {
+    quote:'Be the change that you wish to see in the wolrd.', 
+    source:'Mahtma Gandhi'
+  },
+  {
+    quote:'Without music, life would be a mistake.', 
+    source: 'Friedrich Nietzsche',citation:'Twilight of the Idols'
+  },
+  {
+    quote:'You pray for rain, you gotta deal with the mud too. That is a part of it', 
+    source:'Denzel Washington',
+    year:2014
+  },
+  {
+    quote:'Life is really simple, but men insist on making it complicated',
+    source:'Confucius'
+  }
+]
+
+// create `getRandomQuote` function to get random quotes from array
+
+function getRandomQuote (arr) {
+  //get random item from array. Instead of number use length of an array
+  let randomNum  = Math.floor(Math.random() * arr.length);
+  let arrObj = arr[randomNum]
+    return arrObj
+}
+//call the function 
+// console.log(getRandomQuote(quotes))
 
 
+// create `printQuote` function to print the quotes on a page with all the keys
 
-/***
- * `getRandomQuote` function
-***/
+function printQuote () {
 
+  let rndmQuote = getRandomQuote(quotes)
+  
+  //variable to store future string
+  let message = ''
+      message += `<p class= "quote"> ${rndmQuote.quote} </p>`
+      message += `<p class= "source"> ${rndmQuote.source}`
+    
+    //if statement to check if the quote has a citation
+    if(rndmQuote.citation !== undefined){
+      message += `<span class= "citation"> ${rndmQuote.citation} </span>`
+  }
+  
+  //if statement to check if the quote has a year
+    if(rndmQuote.year !== undefined){
+      message += `<span class= "year"> ${rndmQuote.year} </span>`
+  }
+      message += `</p>`
 
-
-/***
- * `printQuote` function
-***/
-
-
-
+  document.getElementById('quote-box').innerHTML = message;
+  
+}
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
